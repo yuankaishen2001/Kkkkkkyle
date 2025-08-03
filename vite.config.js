b6d0bcdba2c932e889089aa26ec8217b0e824128
Kaishen Yuan <81404489+yuankaishen2001@users.noreply.github.com>
2025-08-03 03:46:39 +0000

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/yuankaishen2001.github.io/',
   plugins: [
     vue(),
     vueDevTools(),
